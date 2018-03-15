dc0dc38d3ec766e9bd0e2a0af81112ee0e09262f
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:22:09 +0100

    new bootstrap

--- src/Handler/HandlerConfigurator.php
+++ src/Handler/HandlerConfigurator.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * kiwi-suite/command-bus (https://github.com/kiwi-suite/command-bus)
+ *
+ * @package kiwi-suite/command-bus
+ * @see https://github.com/kiwi-suite/command-bus
+ * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 namespace KiwiSuite\CommandBus\Handler;
 
